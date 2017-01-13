9f3e36d4c5e22368e51fc2d668afd80578db2708
Viktor Gustavsson <villor94@gmail.com>
2017-01-13 05:55:15 +0100

    Added webpack/babel and base app module

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, 'build');
+
+module.exports = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  loaders: {
+    test: /\.js$/,
+    include: APP_DIR,
+    loader: 'babel-loader',
+    babelrc: true
+  }
+};
