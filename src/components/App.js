09fb9a6083fb52796f13a6c6dbd2d84d367468e4
Viktor Gustavsson <villor94@gmail.com>
2017-01-13 14:58:03 +0100

    Designed lists

--- src/components/App.js
+++ src/components/App.js
@@ -12,20 +12,31 @@ const timeRanges = [
 ];
 
 const Track = (props) => (
-  <li>
-    {props.number + ' ' + props.name}
+  <li className="track">
+    <img src={props.album.images[props.album.images.length - 1].url} alt={props.name} />
+    <div className="list-number">{props.number}</div>
+    <div className="list-names">
+      <div title={props.name}>{props.name}</div>
+      <div title={props.artists[0].name}>{props.artists[0].name}</div>
+    </div>
   </li>
 );
 
 const Artist = (props) => (
-  <li>
-    {props.number + ' ' + props.name}
-  </li>
+    <li className="artist">
+      { props.images.length ? (
+        <img src={props.images[props.images.length - 1].url} alt={props.name} />
+      ) : (
+        <img src="" />
+      )}
+      <div className="list-number">{props.number}</div>
+      <div className="list-names" title={props.name}>{props.name}</div>
+    </li>
 );
 
 const List = (props) => (
   <div className="list-container">
-    <h3>{props.title}</h3>
+    <h2>{props.title}</h2>
     <ol>
       {props.dataArray.map((el, i) => <props.type number={i + 1} key={i + 1} {...el} />)}
     </ol>
@@ -78,7 +89,7 @@ export default class App extends React.Component {
     render() {
       return (
         <div>
-          <h1>Spotify User Top</h1>
+          <h1 className="title">User Stats for Spotify</h1>
           <div>
             <Selector values={timeRanges} value={this.state.timeRange} handleChange={this.handleSelectorChange} />
           </div>
