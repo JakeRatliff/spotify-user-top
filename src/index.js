2b4a498aa4bd0fb92398b7271af5ac13147ee553
Viktor Gustavsson <villor94@gmail.com>
2017-01-13 06:22:57 +0100

    Added React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-document.body.innerHTML += "It works";
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App.js';
+
+render(<App />, document.getElementById('app'));
